497fa118a430f835d8b2aefae69fa97d35c7604b
Omkar Palav <113368893+Omkarpalav@users.noreply.github.com>
2024-12-18 18:22:17 -1200

    changes

--- function.js
+++ function.js
@@ -29,3 +29,24 @@ function lastName (last = "palav") {
 }
 console.log(lastName());
 console.log(lastName("Parab"));
+
+// All function Example in one code 
+// 1. function Declaration 
+function abc(x,y) {
+    return x * y;
+}
+console.log(`The multipliaction of A and B is : `, abc(2,3));
+
+// 2. Expression Function
+const sum = function sumNumber(num1, num2) {
+    return num1 + num2;
+}
+console.log(`Sum of Two number is : `, sum(100,200));
+
+// 3. Arrow Function
+const result = (name) => {
+    return `Hello ${name}`;
+}
+
+console.log(result("omkar"));
+
